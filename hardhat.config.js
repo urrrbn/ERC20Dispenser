64bf223c0e6e41ef431770fbbd13806eb554fc6a
urrrbn <urbnannamaria@gmail.com>
2024-05-13 15:51:06 +0200

    🎉 Start
    Scaffolding

---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.SEPOLIA_API_KEY}`,
+      accounts: [`${process.env.DEPLOYER_PRIVATE_KEY}`], 
+      allowUnlimitedContractSize: true
+    }
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: {
+          optimizer: {
+            enabled: true, 
+            runs: 200     
+          }
+        },
+      },
+    ],
+  }
+  
+};
