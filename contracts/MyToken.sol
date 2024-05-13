18a3917a4533209c55100749a08849370f5c5d6f
urrrbn <urbnannamaria@gmail.com>
2024-05-13 17:44:14 +0200

    🍱 Add MyToken.sol

---
+++ contracts/MyToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 700_000e18);
+    }
+}
